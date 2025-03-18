e0b505a329907261314f29bdff826006fd8ab600
Nandita <129239412+NReddy-15@users.noreply.github.com>
2025-03-18 12:21:06 -0400

    Implemented onSaveInstanceState to retain fragment state

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -15,6 +15,8 @@ class DieFragment : Fragment() {
     lateinit var dieTextView: TextView
 
     var dieSides: Int = 6
+    private val CURRENT_ROLL_KEY = "current roll"
+    private var current_roll = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +39,29 @@ class DieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
-        view.setOnClickListener{
+        //throwDie()
+        savedInstanceState?.run {
+            current_roll = getInt(CURRENT_ROLL_KEY, 0)
+        }
+        if (current_roll == 0) {
             throwDie()
+        }
+        else {
+            throwDie(current_roll)
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(CURRENT_ROLL_KEY, current_roll)
+    }
+
     fun throwDie() {
-        dieTextView.text = (Random.nextInt(dieSides)+1).toString()
+        current_roll = (Random.nextInt(dieSides)+1)
+        dieTextView.text = current_roll.toString()
+    }
+
+    fun throwDie(num: Int) {
+        dieTextView.text = num.toString()
     }
 }
